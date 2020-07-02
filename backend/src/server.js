160972e3c8ac20f93eab294656673790b3bff498
Syond <syond94@gmail.com>
2020-07-02 15:36:09 -0300

    Routes and enviroment variables config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express   = require("express");
+const cors      = require('cors');
+const routes    = require('./routes');
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(cors());
+app.use(routes);
 
 app.listen(process.env.PORT || 3000, process.env.HOST || "localhost");
