b418a0b7c2ff8088ea9f872ddb41ba313725eac2
Syond <syond94@gmail.com>
2020-08-28 17:29:51 -0300

    finished user tests and functions implementations.

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -1,30 +1,43 @@
 const User = require("../models/User");
-const { password } = require("../../config/database");
 
 module.exports = class UserController {
   async store(request, response) {
-    const { email } = request.body;
+    const { email, password, name } = request.body;
 
-    if (await User.findOne({ where: { email } }))
-      return response.status(400).send({ error: "User already registred." });
+    if (!email)
+      return response.status(400).send({ error: "email is required" });
+    if (!password)
+      return response.status(400).send({ error: "password is required" });
+    if (!name) return response.status(400).send({ error: "name is required" });
 
-    const user = await User.create(request.body);
+    try {
+      if (await User.findOne({ where: { email } }))
+        return response.status(400).send({ error: "User already registred." });
 
-    response.status(200).json(user);
+      const user = await User.create(request.body);
+
+      return response.status(201).json(user);
+    } catch (error) {
+      return response.status(400).send({ error: "Something went wrong." });
+    }
   }
 
   async update(request, response) {
     const { name, password } = request.body;
 
-    const user = await User.findByPk(request.params.id);
+    try {
+      const user = await User.findByPk(request.params.id);
 
-    if (!user) return response.status(400).send({ error: "User not found." });
+      if (!user) return response.status(400).send({ error: "User not found." });
 
-    user.name = name;
-    user.password = password;
+      user.name = name;
+      user.password = password;
 
-    await user.save();
+      await user.save();
 
-    response.status(200).json(user);
+      return response.status(200).json(user);
+    } catch (error) {
+      return response.status(400).send({ error: "Something went wrong." });
+    }
   }
 };
--- backend/tests/integration/user.test.js
+++ backend/tests/integration/user.test.js
@@ -11,6 +11,44 @@ describe("User Controller", () => {
   });
 
   describe("store", () => {
+    it("should return status 400 when email is not provided", async () => {
+      const userWithoutEmail = {
+        name: "Josley Souza",
+        email: undefined,
+        password: "12345",
+      };
+
+      const response = await request(app).post("/users").send(userWithoutEmail);
+
+      expect(response.status).toBe(400);
+    });
+
+    it("should return status 400 when password is not provided", async () => {
+      const userWithoutPassword = {
+        name: "Josley Souza",
+        email: "josley@gmail.com",
+        password: undefined,
+      };
+
+      const response = await request(app)
+        .post("/users")
+        .send(userWithoutPassword);
+
+      expect(response.status).toBe(400);
+    });
+
+    it("should return status 400 when name is not provided", async () => {
+      const userWithoutName = {
+        name: undefined,
+        email: "josley@gmail.com",
+        password: "51561",
+      };
+
+      const response = await request(app).post("/users").send(userWithoutName);
+
+      expect(response.status).toBe(400);
+    });
+
     it("should encrypt the user password when new user is created", async () => {
       const user = await factory.create("User", {
         password_testHash: "123456",
@@ -25,26 +63,54 @@ describe("User Controller", () => {
       //Using "attrs" function to not create a instance of User inside the database
       const user = await factory.attrs("User");
 
-      const response = await request(app).post("/users").send(user);
+      const userPasswordAttrChange = {
+        ...user,
+        password: user.password_testHash,
+      };
+
+      const response = await request(app)
+        .post("/users")
+        .send(userPasswordAttrChange);
 
       expect(response.body).toHaveProperty("id");
     });
 
-    it("should return status 200 when new user is created", async () => {
+    it("should return status 201 when new user is created", async () => {
       //Using "attrs" function to not create a instance of User inside the database
       const user = await factory.attrs("User");
 
-      const response = await request(app).post("/users").send(user);
+      const userPasswordAttrChange = {
+        ...user,
+        password: user.password_testHash,
+      };
 
-      expect(response.status).toBe(200);
+      const response = await request(app)
+        .post("/users")
+        .send(userPasswordAttrChange);
+
+      expect(response.status).toBe(201);
     });
 
     it("should not be able to create new user with same email", async () => {
       const user = await factory.attrs("User");
+      const anotherUser = await factory.attrs("User");
 
-      await request(app).post("/users").send(user);
+      const userPasswordAttrChange = {
+        ...user,
+        password: user.password_testHash,
+      };
 
-      const response = await request(app).post("/users").send(user);
+      await request(app).post("/users").send(userPasswordAttrChange);
+
+      const userWithSameEmail = {
+        ...anotherUser,
+        password: anotherUser.password_testHash,
+        email: userPasswordAttrChange.email,
+      };
+
+      const response = await request(app)
+        .post("/users")
+        .send(userWithSameEmail);
 
       expect(response.status).toBe(400);
     });
@@ -54,7 +120,14 @@ describe("User Controller", () => {
     it("should be able to update existing user", async () => {
       const user = await factory.attrs("User");
 
-      const responseCreate = await request(app).post("/users").send(user);
+      const userPasswordAttrChange = {
+        ...user,
+        password: user.password_testHash,
+      };
+
+      const responseCreate = await request(app)
+        .post("/users")
+        .send(userPasswordAttrChange);
 
       const updatedUser = {
         ...responseCreate.body,
@@ -72,7 +145,14 @@ describe("User Controller", () => {
     it("should return status 200 when the user is updated", async () => {
       const user = await factory.attrs("User");
 
-      const responseCreate = await request(app).post("/users").send(user);
+      const userPasswordAttrChange = {
+        ...user,
+        password: user.password_testHash,
+      };
+
+      const responseCreate = await request(app)
+        .post("/users")
+        .send(userPasswordAttrChange);
 
       const updatedUser = {
         ...responseCreate.body,
@@ -87,7 +167,7 @@ describe("User Controller", () => {
       expect(responseUpdate.status).toBe(200);
     });
 
-    it("should return status 400 when the user not exist", async () => {
+    it("should return status 400 if the user not exist", async () => {
       const response = await request(app).put("/users/5");
 
       expect(response.status).toBe(400);
